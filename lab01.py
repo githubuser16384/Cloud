50c2158c82c7ecd9175627d3c3a48c655f5f7d09
githubuser16384 <117111471+githubuser16384@users.noreply.github.com>
2024-08-26 13:39:05 +0530

    Update lab01.py

--- lab01.py
+++ lab01.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn import linear_model
-height=[[4.0],[5.0],[6.0],[7.0],[8.0],[9.0],[10.0],[11.0]
+height=[[4.0],[5.0],[6.0],[7.0],[8.0],[9.0],[10.0],[11.0]]
 weight=[  8, 10 , 12, 14, 16, 18, 20,22]
 reg=linear_model.LinearRegression()
 reg.fit(height,weight)
